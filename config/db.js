7be7c44e5e25c1d8f50e84bcc281dbd28c20112a
Mauricio <duqueivan493@gmail.com>
2024-08-25 14:02:36 -0500

    cambios .env y db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
-import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
-dotenv.config({path:'.env'})
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.BD_HOST,
+dotenv.config();  // Cargar variables del archivo .env
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
-        timestamps: true
+        timestamps: true,
     },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorAliases: false
 });
+
+try {
+    await db.authenticate();
+    console.log('Conexión a la base de datos establecida correctamente.');
+} catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+}
+
 export default db;
-// 
